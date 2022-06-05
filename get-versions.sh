6aad3e64226861bf839df881e1d6c81321e94636
Akiicat <aaaa1379@gmail.com>
2022-06-05 15:13:35 +0800

    test auto git

--- get-versions.sh
+++ get-versions.sh
@@ -1,30 +1,68 @@
 #!/bin/bash
 
-versions=$(curl -sS -X "GET" https://multipaper.io/api/v2/projects/multipaper | jq -r ".versions[]")
-versions=($versions)
 
+# docker_hun_server_repo=${
 
+github_server_tags=$(wget -q https://registry.hub.docker.com/v1/repositories/akiicat/test/tags -O -)
+# docker_hub_server_tags=$(wget -q https://registry.hub.docker.com/v1/repositories/akiicat/test/tags -O -)
+# docker_hub_master_tags=$(wget -q https://registry.hub.docker.com/v1/repositories/akiicat/test/tags -O -)
+
+versions=($(curl -sS -X "GET" https://multipaper.io/api/v2/projects/multipaper | jq -r ".versions[]"))
 for version in "${versions[@]}"; do
-        echo "-> " $version
-
-        builds=$(curl -sS -X "GET" https://multipaper.io/api/v2/projects/multipaper/versions/$version | jq -r ".builds[]")
-        builds=($builds)
+        builds=($(curl -sS -X "GET" https://multipaper.io/api/v2/projects/multipaper/versions/$version | jq -r ".builds[]"))
 
         for build in "${builds[@]}"; do
-                echo "---> " $build
+                echo "-> $version-$build"
 
-                break
+                # is_exist=$(echo $docker_hub_server_tags | jq -r ".[].name" | grep "$version-$build")
+                is_exist=$(echo $github_server_tags | jq -r ".[].name" | grep "$version-$build")
+
+                if [ -n "$is_exist" ]; then
+                        continue;
+                fi
+
+                set -x
+
+                downloads=$(curl -sS -X "GET" https://multipaper.io/api/v2/projects/multipaper/versions/$version/builds/$build)
+
+                master_jar=$(echo $downloads | jq -r ".downloads[].name" | grep -i    "master")
+                server_jar=$(echo $downloads | jq -r ".downloads[].name" | grep -i -v "master")
+
+                if [[ $master_jar =~ ([[:digit:]]+.[[:digit:]]+.[[:digit:]]+) ]]; then
+                        master_version=${BASH_REMATCH[1]}
+                fi
+                server_version=$version-$build
+
+                echo "Master Jar: $master_jar"
+                echo "Server Jar: $server_jar"
+                echo "master_version: $master_version"
+                echo "server_version: $server_version"
+
+                mkdir -p server
+                mkdir -p master
+                sed -e "s/{version}/$version/g" -e "s/{build}/$build/g" -e "s/{jar_file}/$server_jar/g" templates/Dockerfile.server > server/Dockerfile
+                sed -e "s/{version}/$version/g" -e "s/{build}/$build/g" -e "s/{jar_file}/$master_jar/g" templates/Dockerfile.master > master/Dockerfile
+                echo "$server_version" > server/tags
+                echo "$master_version" > master/tags
+
+                if [[ -n "$GITHUB_ENV" ]]; then
+                        echo "VERSION=$version" >> $GITHUB_ENV
+                        echo "BUILD=$build" >> $GITHUB_ENV
+                        echo "SERVER_VERSION=$server_version" >> $GITHUB_ENV
+                        echo "MASTER_VERSION=$master_version" >> $GITHUB_ENV
+                fi
+
+                exit
         done
-        break
 done
 
-git branch -a
-git tag
-
-echo "$version $build"
-
-echo "$version" > version
-echo "$build" > build
+# git branch -a
+# git tag
+#
+# echo "$version $build"
+#
+# echo "$version" > version
+# echo "$build" > build
 
 # export version=$version
 # export build=$build
